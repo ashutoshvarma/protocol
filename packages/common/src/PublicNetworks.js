0b2e0ad3bf6285267a206a03b560acb23b24a548
Ashutosh Varma <ashutoshvarma11@live.com>
2021-04-07 01:06:54 +0530

    update public networks

--- packages/common/src/PublicNetworks.js
+++ packages/common/src/PublicNetworks.js
@@ -1,10 +1,10 @@
 const PublicNetworks = {
   1: {
-    name: "mainnet",
-    ethFaucet: null,
-    etherscan: "https://etherscan.io/",
-    daiAddress: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-    wethAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    name: "mainnet0",
+    // ethFaucet: null,
+    // etherscan: "https://etherscan.io/",
+    // daiAddress: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    // wethAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
   },
   3: {
     name: "ropsten",
